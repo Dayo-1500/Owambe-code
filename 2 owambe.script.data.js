aa8ae1489ebf5e2f5e2190c2946008f6fde03cd7
Ekundayo Abdul <71127537+Dayo-45@users.noreply.github.com>
2020-10-21 18:30:29 +0100

    Update 2 owambe.script.data.js

--- 2 owambe.script.data.js
+++ 2 owambe.script.data.js
@@ -1,53 +1,38 @@
-
-/**
- * Copyright 2017 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 'use strict';
 
-FriendlyEats.prototype.addRestaurant = function(data) {
+Owambe.prototype.addUsers = function(data) {
   /*
     TODO: Implement adding a document
   */
+
 };
 
-FriendlyEats.prototype.getAllRestaurants = function(renderer) {
+Owambe.prototype.getUsers = function(renderer) {
   /*
-    TODO: Retrieve list of restaurants
+    TODO: Retrieve list of users
   */
 };
 
-FriendlyEats.prototype.getDocumentsInQuery = function(query, renderer) {
+Owambe.prototype.getDocumentsInQuery = function(query, renderer) {
   /*
     TODO: Render all documents in the provided query
   */
 };
 
-FriendlyEats.prototype.getRestaurant = function(id) {
+Owambe.prototype.getUsers = function(id) {
   /*
-    TODO: Retrieve a single restaurant
+    TODO: Retrieve a single user
   */
 };
 
-FriendlyEats.prototype.getFilteredRestaurants = function(filters, renderer) {
+Owambe.prototype.getFilteredUsers = function(filters, renderer) {
   /*
-    TODO: Retrieve filtered list of restaurants
+    TODO: Retrieve filtered list of users
   */
 };
 
-FriendlyEats.prototype.addRating = function(restaurantID, rating) {
+Owambe.prototype.addrating = function(userID, rating) {
   /*
-    TODO: Retrieve add a rating to a restaurant
+    TODO: Retrieve add a rating to a user
   */
 };
