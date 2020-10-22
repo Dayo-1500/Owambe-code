4594d3e698dcfed36031d2b21d551e1584ed1e77
Ekundayo Abdul <71127537+Dayo-45@users.noreply.github.com>
2020-10-22 16:15:26 +0100

    Update 4 Owambe.script.view.js

--- 4 Owambe.script.view.js
+++ 4 Owambe.script.view.js
@@ -1,18 +1,3 @@
-/**
- * Copyright 2017 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 'use strict';
 
 Owambe.prototype.initTemplates = function() {
@@ -30,7 +15,7 @@ Owambe.prototype.viewHome = function() {
 
 Owambe.prototype.viewList = function(filters, filter_description) {
   if (!filter_description) {
-    filter_description = 'Let's join hands to make things happen.';
+    filter_description = 'Let us join hands to make things happen.';
   }
 
   var mainEl = this.renderTemplate('main-adjusted');
