2dd87fca265b1acbdb326da5526c8685a5c81242
Ekundayo Abdul <71127537+Dayo-45@users.noreply.github.com>
2020-10-22 15:18:57 +0100

    Update 2 owambe.script.data.js

--- 2 owambe.script.data.js
+++ 2 owambe.script.data.js
@@ -74,4 +74,23 @@ Owambe.prototype.addRating = function(userID, rating) {
   /*
     TODO: Retrieve add a rating to users
   */
+  var collection = firebase.firestore().collection('users');
+  var document = collection.doc(userID);
+  var newRatingDocument = document.collection('ratings').doc();
+
+  return firebase.firestore().runTransaction(function(transaction) {
+    return transaction.get(document).then(function(doc) {
+      var data = doc.data();
+
+      var newAverage =
+          (data.numRatings * data.avgRating + rating.rating) /
+          (data.numRatings + 1);
+      
+      transaction.update(document, {
+        numRatings: data.numRatings + 1,
+        avgRating: newAverage
+      });
+      return transaction.set(newRatingDocument, rating);
+    });
+  });
 };
